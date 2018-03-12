869dd3e752b93f57253acd6c95a9d50a1a71299c
Jaroor Modi <jaroorm96@gmail.com>
2018-03-12 19:06:27 -0400

    created indexer, has counting, sorting, tokenizing functionality. Needs file io, currently takes strings args as input.

---
+++ indexer.c
@@ -0,0 +1,306 @@
+//
+//  stringsorter.c
+//
+//
+//  Created by Jaroor Modi on 1/24/18.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum relative_position {BEFORE, AFTER, CLONE} relation;
+
+int is_uppercase (char letter){
+	//	Tests whether a character is a uppercase letter.
+	if('A' <= letter && letter <= 'Z'){
+		return 1;
+	}
+	return 0;
+}
+
+int is_lowercase (char letter){
+	//	Tests whether a character is an lowercase letter.
+	if('a' <= letter && letter <= 'z'){
+		return 1;
+	}
+	return 0;
+}
+
+int is_numeric (char numeral){
+	//	Tests whether a character is a numerical character.
+	if('0' <= numeral && numeral <= '9'){
+		return 1;
+	}
+	return 0;
+}
+
+int is_accepted (char letter){
+	//	Simply returns whether or not a character is either a letter or a number
+	if(is_lowercase(letter) || is_uppercase(letter) || is_numeric(letter)){
+		return 1;
+	}
+	return 0;
+}
+
+int num_words (char * in_string){
+	//	Counts the number of words (sets of consecutive alphabetical characters),
+	//	used for memory allocation and giving array sizes.
+	int word_count = 0;
+	int position = 0;
+	char prev_char = '{';
+	while (in_string[position] != 0){
+		if((is_accepted(in_string[position])) && (!(is_accepted(prev_char)))){
+			word_count++;
+		}
+		prev_char = in_string[position];
+		//printf("\n%c  WORD: %d  ACCEPT? %d", prev_char, word_count, is_accepted(prev_char));
+		position++;
+	}
+	return word_count;
+}
+
+char process_letter(char letter){
+	//	Returns lowercase version of the letter and leaves unchanged if not a letter
+	if (is_lowercase(letter) || is_numeric(letter)){
+		return letter;
+	}
+	else if (is_uppercase(letter)){
+		return letter+32;
+	}
+	else{
+		return '-';
+	}
+}
+
+char * normalize_string(char * in_string){
+	//	Normalizes input string for separation: turns all non-alphabetical characters
+	//	into the given delimiter for use in strtok operations later in the process.
+	int length,position;
+	length = strlen(in_string);
+	position = 0;
+	
+	char * out_string = malloc(sizeof(char) * length);
+	strcpy(out_string, in_string);
+	
+	while(out_string[position]!=0){
+		out_string[position] = process_letter(out_string[position]);
+		position++;
+	}
+	
+	printf("\n%s\n", out_string);
+	
+	return out_string;
+}
+
+char ** separate_string(char * in_string){
+	//	Returns an array of strings (char * variables), each of which is a single word found in the
+	//	input string. Here, "word" refers to a set of consecutive characters which are found in the
+	//	English alphabet.
+	int word_count, position;
+	word_count = num_words(in_string);
+	printf("\nWORD COUNT:   %d\n", word_count);
+	position = 0;
+	char * interim = normalize_string(in_string);
+	char ** word_array = malloc(sizeof(char*) * (word_count + 1));
+	
+	word_array[position] = strtok(interim,"-");
+	
+	for(position=1; position <= word_count; position++){
+		word_array[position] = strtok(NULL,"-");
+	}
+	
+	*(word_array + word_count) = 0;
+	
+	return word_array;
+}
+
+typedef struct word_node{
+	//	Nodes know the word which they represent and the node which follows in the linked list.
+	char * word;
+	int count;
+	struct word_node * prev;
+	struct word_node * next;
+}node_t;
+
+node_t * new_node(char * wordd){
+	node_t * ret_node = malloc(sizeof(node_t));
+	ret_node->word = wordd;
+	ret_node->count = 1;
+	ret_node->prev = NULL;
+	ret_node->next = NULL;
+	return ret_node;
+}
+
+typedef struct node_list{
+	//	This is the "list" structure which is the primary structure which the functions below work with.
+	//	This was created to make tracking the head of our linked list manageable.
+	node_t * head;
+}list_t;
+
+relation comes_first(char * new_word, char * old_word){
+	//	Returns 1 if new_word sorts before old_word, 0 otherwise. Used in the insert_node function
+	//	where the new_word field is the new node to insert and the old_word field will be used for
+	//	the preexisting nodes in the linked list as we iterate through.
+	int position;
+	if(strcmp(new_word,old_word) == 0){
+		//		If the new word is the same as the old, we say it is a "clone" for counting
+		return CLONE;
+	}
+	position = 0;
+	//compare character by character
+	while(new_word[position] != 0 && old_word[position] != 0){
+		//both chars numeric
+		if ((is_numeric(new_word[position])) && (is_numeric(old_word[position]))){
+			if(new_word[position]<old_word[position]){
+				//printf("case 1 %s BEFORE %s\n", new_word, old_word);
+				return BEFORE;
+			}
+			if(new_word[position]>old_word[position]){
+				return AFTER;
+			}
+		}
+		//only new word char is numeric
+		else if ((is_numeric(new_word[position])) && !(is_numeric(old_word[position]))){
+			return AFTER;
+		}
+		
+		//only old word char is numeric
+		else if (!(is_numeric(new_word[position])) && (is_numeric(old_word[position]))){
+			//printf("case 2 %s BEFORE %s\n", new_word, old_word);
+			return BEFORE;
+		}
+		
+		//neither char is numeric
+		else{
+			if(new_word[position]<old_word[position]){
+				//printf("case 3 %s BEFORE %s\n", new_word, old_word);
+				//printf("because %c is LESS THAN %c\n", new_word[position], old_word[position]);
+				return BEFORE;
+			}
+			if(new_word[position]>old_word[position]){
+				return AFTER;
+			}
+		}
+		position ++;
+	}
+	//everything compared so far is equivalent, so we just put the shorter word first
+	if(strlen(old_word) > strlen(new_word)){
+		return BEFORE;
+	}
+	return AFTER;
+}
+
+void insert_node(list_t * list, char * wordd){
+	//	Inserts nodes into our linked list structure but in the proper, sorted order using the
+	//	comes_first comparison function to find the word's relative position in the list.
+	node_t * new_word = new_node(wordd);
+	if(list->head == NULL){
+		//printf("Inserted new head: %s\n", new_word->word);
+		list->head = new_word;
+		return;
+	}
+	node_t * current = list->head;
+	while (current!=NULL){
+		if(comes_first(new_word->word, current->word) == CLONE){
+			current->count += 1;
+			free(new_word);
+			return;
+		}
+		current = current->next;
+	}
+	current = list->head;
+	while (current->next!=NULL){
+		switch(comes_first(new_word->word, current->word)){
+			case BEFORE:
+				//printf("\n placed %s BEFORE %s\n", new_word->word, current->word);
+				if (current->prev == NULL){
+					//printf("inserted new head: %s\n", new_word->word);
+					new_word->next = current;
+					current->prev = new_word;
+					new_word->prev = NULL;
+					list->head = new_word;
+					return;
+				}
+				new_word->prev = current->prev;
+				current->prev->next = new_word;
+				new_word->next = current;
+				current->prev = new_word;
+				return;
+			case CLONE:
+			case AFTER:
+				break;
+		}
+		current = current->next;
+	}
+	//add to end if it doesn't come before any of the words in the list
+	current->next = new_word;
+	new_word->prev = current;
+	return;
+}
+
+list_t * sort_list (char ** word_array){
+	//	Creates an empty list and inserts nodes using the insert_node function defined above. This
+	//	results in a linked list with the nodes such that the words they contain are sorted from head to tail.
+	list_t * ret_list = malloc(sizeof(list_t));
+	ret_list->head = NULL;
+	int position = 0;
+	while(word_array[position]!=0){
+		insert_node(ret_list, word_array[position]);
+		position++;
+	}
+	return ret_list;
+}
+
+void print_list(list_t * sorted){
+	//	Prints input linked list. The list is sorted by the time this function is called,
+	//	this completes our program.
+	node_t * current = sorted->head;
+	while(current != NULL){
+		printf("%s, %d\n",current->word, current->count);
+		current = current->next;
+	}
+	return;
+}
+
+int main (int argc, char * argv[]){
+	if (argc<2){
+		printf("You must input at least one string to run this program.\nRun using the format ./stringsorter \"<your input string here>\"\n");
+		return 0;
+	}
+	if(argc>2){
+		printf("No more than one input string allowed at a time.\nRun using the format ./stringsorter \"<your input string here>\"\n");
+		return 0;
+	}
+	
+	int length = strlen(argv[1]);
+	char * input_string = malloc(sizeof(char)*length);
+	strcpy(input_string,argv[1]);
+	char ** split_words = separate_string(input_string);
+	list_t * sorted = sort_list(split_words);
+	print_list(sorted);
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
